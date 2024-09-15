234c084a9f8a3b3a19946f36f77a20ac490f7f6c
Sebastian Giraldo <jusegiraldo76@gmail.com>
2024-09-14 21:52:44 -0500

    perf: :art: new interfaz

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-const FORM = document.getElementById("formulario");
-const RESULT = document.getElementById("resultado");
+document.addEventListener("DOMContentLoaded", () => {
+  const FORM = document.getElementById("formulario");
+  const RESULT = document.getElementById("resultado");
+  const RESET_BUTTON = document.getElementById("reset");
 
-FORM.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let initialPercentage = document.getElementById("porcentaje").value;
-  let productValue = document.getElementById("valorproducto").value;
-  handleGanancia(initialPercentage, productValue);
+  FORM.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let initialPercentage = document.getElementById("porcentaje").value;
+    let productValue = document.getElementById("valorproducto").value;
+    handleGanancia(initialPercentage, productValue);
+  });
+
+  //funcion de la calculadora
+  const handleGanancia = (percetange, value) => {
+    initialPercentage = Number(percetange);
+    productValue = Number(value);
+
+    const FIRSTSTEP = 100 - initialPercentage;
+
+    let partialProductValue = (productValue * FIRSTSTEP) / 100;
+    let finalProductValue = productValue + partialProductValue;
+
+    if (initialPercentage === 0 || finalProductValue === 0) {
+      RESULT.innerHTML = "Introduce los valores correspondientes.";
+    } else {
+      const FORMATTEDVALUE = finalProductValue.toLocaleString("es-co", {
+        style: "currency",
+        currency: "COP",
+        minimumFractionDigits: 0,
+      });
+      RESULT.innerHTML = FORMATTEDVALUE;
+    }
+  };
+
+  const resetInputs = () => {
+    FORM.reset();
+    RESULT.innerHTML = "";
+  };
+
+  RESET_BUTTON.addEventListener("click", () => {
+    resetInputs();
+  });
 });
-
-//funcion de la calculadora
-const handleGanancia = (percetange, value) => {
-  initialPercentage = Number(percetange);
-  productValue = Number(value);
-
-  const FIRSTSTEP = 100 - initialPercentage;
-
-  let partialProductValue = (productValue * FIRSTSTEP) / 100;
-  let finalProductValue = productValue + partialProductValue;
-
-  if (initialPercentage === 0 || finalProductValue === 0) {
-    RESULT.innerHTML = "Introduce los valores correspondientes.";
-  } else {
-    const FORMATTEDVALUE = finalProductValue.toLocaleString("es-co", {
-      style: "currency",
-      currency: "COP",
-      minimumFractionDigits: 0,
-    });
-    RESULT.innerHTML = FORMATTEDVALUE;
-  }
-};
